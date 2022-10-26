c678d72977580ddf106d507cff82b0cefbe9a5a2
Męczyciel Biedronek <44778546+MeczycielBiedronek@users.noreply.github.com>
2022-10-26 21:40:35 +0200

    style: add color to sequence blocks

--- app.js
+++ app.js
@@ -2,51 +2,36 @@
 const wrapper = document.getElementById("wrapper");
 const shape = document.querySelectorAll(".shape");
 
-// shape.forEach((element) => {
-//   element.addEventListener('click', ()=>{
-//     let color = element.style.color
-//     element.style.backgroundColor = "red"
-//     setInterval(()=>{
-//       element.style.backgroundColor = color
-//     }, 2000)
-// })
-// })
-
-
-// const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
-// const uniqueRand = (min, max, prev) => {
-//   let next = prev;
-  
-//   while(prev === next) next = rand(min, max);
-  
-//   return next;
-// }
+// { configuration: 3, roundness: 3 }, // idea 1
+// { configuration: 1, roundness: 4 },
+// { configuration: 2, roundness: 2 },
+// { configuration: 1, roundness: 2 }, // develop 4
+// { configuration: 2, roundness: 3 }
 
 const combinations = [
   // { configuration: 1, roundness: 1 },
-  { configuration: 3, roundness: 3 }, // idea 1
-  { configuration: 1, roundness: 4 },
-  { configuration: 2, roundness: 2 },
-  { configuration: 1, roundness: 2 }, // develop 4
-  { configuration: 2, roundness: 3 }
+  { configuration: 1, roundness: 3 }, // idea 1
+  { configuration: 2, roundness: 4 },
+  { configuration: 3, roundness: 2 },
+  { configuration: 4, roundness: 2 }, // develop 4
+  { configuration: 5, roundness: 3 }
 
 ];
-let index = 0;
+let index = 4;
 wrapper.dataset.configuration = combinations[index].configuration;
 wrapper.dataset.roundness = combinations[index].roundness
 
 
 
-setInterval(() => {
+// setInterval(() => {
 
-  let combination = combinations[index];
+//   let combination = combinations[index];
   
-  wrapper.dataset.configuration = combination.configuration;
-  wrapper.dataset.roundness = combination.roundness;
-  console.log(index)
-  index ++;
-  if (index == 5) {
-    index = 0
-  }
-}, 3000);
+//   wrapper.dataset.configuration = combination.configuration;
+//   wrapper.dataset.roundness = combination.roundness;
+//   console.log(index)
+//   index ++;
+//   if (index == 5) {
+//     index = 0
+//   }
+// }, 4000);
