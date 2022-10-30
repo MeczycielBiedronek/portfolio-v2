89372262e314d3a648675d0017a9bf2f5c613abf
Męczyciel Biedronek <44778546+MeczycielBiedronek@users.noreply.github.com>
2022-10-30 23:22:23 +0100

    feat: add intro animation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 "use strict"
 const wrapper = document.getElementById("wrapper");
+const intro = document.getElementById("intro");
 const shape = document.querySelectorAll(".shape");
+const skip = document.querySelector(".skip");
 
 // { configuration: 3, roundness: 3 }, // idea 1
 // { configuration: 1, roundness: 4 },
@@ -14,7 +16,7 @@ const combinations = [
   { configuration: 2, roundness: 4 },
   { configuration: 3, roundness: 2 },
   { configuration: 4, roundness: 2 }, // develop 4
-  { configuration: 5, roundness: 3 }
+  { configuration: 5, roundness: 5 }
 
 ];
 
@@ -29,14 +31,19 @@ setInterval(() => {
   
   wrapper.dataset.configuration = combination.configuration;
   wrapper.dataset.roundness = combination.roundness;
-  console.log(index)
   index ++;
   if (index == 5) {
     index = 0
+    skip.style.color = "#333333"
+    // skip.innerHTML = "next"
   }
 }, 4000);
 
 // Opcja statyczna
-// let index = 3;
+// let index = 4;
 // wrapper.dataset.configuration = combinations[index].configuration;
 // wrapper.dataset.roundness = combinations[index].roundness
+
+intro.addEventListener('click', ()=> {
+  intro.setAttribute('status', 'off')
+})
